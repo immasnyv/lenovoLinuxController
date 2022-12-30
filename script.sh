f82f95fc44dde98986e76aae58c097159a630fb6
Vojcztek <47929630+immasnyv@users.noreply.github.com>
2022-12-30 21:51:54 +0100

    Script and applet configuration

---
+++ script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Linux based Lenovo Thinkbook profile changer V1.0
+# Intended for use with bash sensors widget in linux mint
+# Author: Vojta
+
+state=$(</sys/firmware/acpi/platform_profile)
+echo "Switched to: "
+
+if [ $state = "performance" ]
+then
+pkexec bash -c 'echo "balanced" > /sys/firmware/acpi/platform_profile'
+echo "Balanced"
+elif [ $state = "balanced" ]
+then
+pkexec bash -c 'echo "low-power" > /sys/firmware/acpi/platform_profile'
+echo "Low power"
+else
+pkexec bash -c 'echo "performance" > /sys/firmware/acpi/platform_profile'
+echo "Performance"
+fi
